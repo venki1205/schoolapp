ee8b4afe6f7a702be86bd1c31d9b905da34c4fc7
venki1205 <venkatesh.errolla1205@gmail.com>
2025-02-10 16:18:14 +0530

    dashboard component created and path added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+  export class LoginComponent {
+    loginForm: FormGroup;
+  
+    constructor( private authService: AuthService,  private router: Router) {
+    this.loginForm= new FormGroup({
+      email: new FormControl(),
+      password: new FormControl()
+    })
+    
+  }
+
+  login(){
+    console.log(this.loginForm);
+    this.authService.login(this.loginForm.value).subscribe((data:any)=>{
+      localStorage.setItem('token',data.token);
+      console.log(data);
+      this.router.navigate(['/dashboard']);
+      alert("Login Success")
+      
+      
+    },(err:any)=>{
+      alert("Login Failed")
+    }
+    )
+   
+  }
 
 }
