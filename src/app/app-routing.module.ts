eb6b926e3d4639a79e747c090c30b5f5c7689d73
venki1205 <venkatesh.errolla1205@gmail.com>
2025-02-10 16:29:51 +0530

    AuthGuard Implimentation Completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   {path:'login',component:LoginComponent},
-  {path:'dashboard',component:DashboardComponent}
+  {path:'dashboard',component:DashboardComponent, canActivate:[AuthGuard]}
 ];
 
 @NgModule({
